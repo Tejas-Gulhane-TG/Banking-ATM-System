e81016740d07bf50af4beb60c747c9125cb0f10a
Tejas Gulhane <tgulhane28@gmail.com>
2023-06-13 12:13:35 +0530

    Starting

---
+++ src/main/java/com/example/Banking/System/RestAPI/BankingSystemRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Banking.System.RestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingSystemRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingSystemRestApiApplication.class, args);
+	}
+
+}
